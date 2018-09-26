c972754863b5fba980fa64955acc22d372b46121
20161104567 <846129391@qq.com>
2018-09-26 11:26:07 +0800

    the calculation
    calculator frame

--- 9262/ViewController.swift
+++ 9262/ViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var text: UITextField!
+    @IBAction func zer0(_ sender: Any) {
+    }
+    @IBAction func Delete(_ sender: Any) {
+    }
+    @IBAction func equal(_ sender: Any) {
+    }
+    @IBAction func one(_ sender: Any) {
+    }
+    @IBAction func two(_ sender: Any) {
+    }
+    @IBAction func three(_ sender: Any) {
+    }
+    @IBAction func four(_ sender: Any) {
+    }
+    @IBAction func five(_ sender: Any) {
+    }
+    @IBAction func six(_ sender: Any) {
+    }
+    @IBAction func seven(_ sender: Any) {
+    }
+    @IBAction func eight(_ sender: Any) {
+    }
+    @IBAction func nine(_ sender: Any) {
+    }
+    @IBAction func add(_ sender: Any) {
+    }
+    @IBAction func reduction(_ sender: Any) {
+    }
+    @IBAction func take(_ sender: Any) {
+    }
+    @IBAction func addition(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
