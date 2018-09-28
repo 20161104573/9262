7e69f6c43632c48369397e5fc6348b4578f3ea82
20161104567 <846129391@qq.com>
2018-09-28 17:27:05 +0800

    text
    可以在文本框中显示数字

--- 9262/ViewController.swift
+++ 9262/ViewController.swift
@@ -10,38 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var text: UITextField!
+    
+    @IBOutlet weak var show: UITextField!
     @IBAction func zer0(_ sender: Any) {
+        show.text="0"
     }
     @IBAction func Delete(_ sender: Any) {
+        //show.text=""
     }
     @IBAction func equal(_ sender: Any) {
+        show.text="="
     }
     @IBAction func one(_ sender: Any) {
+        show.text="1"
     }
     @IBAction func two(_ sender: Any) {
+        show.text="2"
     }
     @IBAction func three(_ sender: Any) {
+        show.text="3"
     }
     @IBAction func four(_ sender: Any) {
+        show.text="4"
     }
     @IBAction func five(_ sender: Any) {
+        show.text="5"
     }
     @IBAction func six(_ sender: Any) {
+        show.text="6"
     }
     @IBAction func seven(_ sender: Any) {
+        show.text="7"
     }
     @IBAction func eight(_ sender: Any) {
+        show.text="8"
     }
     @IBAction func nine(_ sender: Any) {
+        show.text="9"
     }
     @IBAction func add(_ sender: Any) {
+        show.text="+"
     }
     @IBAction func reduction(_ sender: Any) {
+        show.text="-"
     }
     @IBAction func take(_ sender: Any) {
+        show.text="*"
     }
     @IBAction func addition(_ sender: Any) {
+        show.text="/"
     }
     override func viewDidLoad() {
         super.viewDidLoad()
